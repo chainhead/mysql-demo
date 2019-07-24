df3a628ab4e690cf807052703df1b8ff9789decb
Nagesh Subrahmanyam <nsubrahm@Nageshs-MacBook-Pro.local>
2019-07-24 17:38:15 +0530

    App changes

--- app/js/demo.js
+++ app/js/demo.js
@@ -19,18 +19,20 @@ function connSetup(conn) {
 //
 demoRouter.get('/demo', function (req, res, next) {
     res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-Request-Id', req.header('X-Nginx-header'))
     //
     let j
     qry.q0001(dbConn, null, (err, resp) => {
         if (err) {
-            logger.error('Q0001  Code: %s Number: %d SQLSTATE: %s Message: %s', err.code, err.errno, err.sqlState, err.sqlMessage)
+            logger.error('Q0001 - Request ID: %s Code: %s Number: %d SQLSTATE: %s Message: %s', req.header('X-Nginx-header'), err.code, err.errno, err.sqlState, err.sqlMessage)
             res.status(502)
             j = JSON.stringify({e:'err'})
             res.send(j)
         } else {
+            logger.info('Q0001 - Request ID: %s', req.header('X-Nginx-header'))
             j = JSON.stringify({
-                r : resp.res[0].Dummy,
-                f : resp.fields[0]
+                r : resp.res,
+                f : resp.fields
             })
             res.status(200)
             res.send(j)
